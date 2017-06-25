fc8ae08914ec5d5ed0ae564214084128e8846f1d
Connor Grady <conair360@gmail.com>
2017-06-25 07:16:19 -0400

    created index.js

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/**
+ * index.js
+ * --------
+ *
+ * @flow
+ */
+
+import Task from './Task'
+import TaskQueue from './TaskQueue'
+
+import type { Task$Executor, Task$options } from './Task'
+import type { TaskQueue$options } from './TaskQueue'
+
+function task(name?: string, executor: Task$Executor, options?: Task$options) : Task {
+    return new Task(name, executor, options);
+}
+function tasks(name?: string, tasks: Array<Task>, options?: TaskQueue$options) : TaskQueue {
+    return new TaskQueue(name, tasks, options);
+}
+
+export default task;
+export {
+    task,
+    tasks,
+    Task,
+    TaskQueue,
+}
