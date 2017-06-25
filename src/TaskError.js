f498e10d9876df813b6f9beb084734d479812d45
Connor Grady <conair360@gmail.com>
2017-06-25 07:16:04 -0400

    created TaskError

---
+++ src/TaskError.js
@@ -0,0 +1,13 @@
+/**
+ * TaskError.js
+ * ------------
+ *
+ * @flow
+ */
+
+import Task from './Task'
+import TaskTrace from './TaskTrace'
+
+class TaskError extends Error {}
+
+export default TaskError;
